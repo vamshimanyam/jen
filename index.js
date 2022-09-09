9cc919d5ac07fb96e55c200d5c07d8a695b7f4a7
vamshimanyam <109961336+vamshimanyam@users.noreply.github.com>
2022-09-09 12:31:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { request } = require('express');
 var express = require('express');
 var app=express();
+const port = process.env.PORT || 3001;
+
 
 app.get("/",(req,res)=>{
     res.send("hello user")
 })
 
-var server=app.listen(5000,function(){
+var server=app.listen(port,function(){
     //console.log("Server Started on port 5000")
 })
 
