75b9ff529ba5a995560e47262273b8e3b851cd05
vamshimanyam <109961336+vamshimanyam@users.noreply.github.com>
2022-09-09 12:54:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { request } = require('express');
 var express = require('express');
 var app=express();
-const port =3001;
+const port = process.env.PORT || 3001;
 
 
 app.get("/",(req,res)=>{
